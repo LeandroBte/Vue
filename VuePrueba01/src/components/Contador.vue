0d6af80301fbe9a1dffc16306cf765ce7bcf51da
Leandro <leandrojosebenitez8@gmail.com>
2024-09-06 20:43:52 -0300

    Creación del componente 'Contador'

---
+++ VuePrueba01/src/components/Contador.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+
+const contador = ref(0);
+
+const incrementar = () =>{
+  contador.value++;
+}
+
+const decrementar = () =>{
+  if(contador.value > 0){
+    contador.value--;
+  }
+}
+
+</script>
+
+<style scoped>
+p{
+  color: red;
+}
+</style>
