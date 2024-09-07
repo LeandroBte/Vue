b30da43017e8997e32ff55745460d4e352e5414d
Leandro <leandrojosebenitez8@gmail.com>
2024-09-07 13:36:27 -0300

    Nuevo commit de prueba.

--- VuePrueba01/src/router/index.js
+++ VuePrueba01/src/router/index.js
@@ -15,7 +15,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../components/Contador.vue')
+      component: () => import('../modules/contador/components/Contador.vue')
     },
     {
       path: '/lista-de-tarea',
@@ -23,7 +23,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../components/ListaDeTarea.vue')
+      component: () => import('../modules/lista de tareas/components/ListaDeTarea.vue')
     }
   ]
 })
